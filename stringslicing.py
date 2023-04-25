d36a4efac7af6880ce1e3d26b6ba8324e171e8ab
Janvipatel1806 <pateljanviaashithbhai@gmail.com>
2023-04-25 16:14:06 +0530

    Third lecture program

---
+++ stringslicing.py
@@ -0,0 +1,14 @@
+s="My name is janvi"
+print(s[1:])
+print(s[0:])
+print(s[5:10])
+print(s[1::3])
+print(s[::4])
+print(s[1::2])
+
+print(s[-15:])
+print(s[::-1])
+print(s[-5:-15:-3])
+print(s[-15:])
+print(s[::])
+
